4fcbf2f1fe05288cb6775aad2ce47e840e194dd8
Matt Campbell <mattandhisapple@gmail.com>
2017-03-12 03:59:44 -0500

    Add Popover Tips

--- js/popover.js
+++ js/popover.js
@@ -1,10 +1,10 @@
 $(function() {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
 
 $(function() {
     $('[data-toggle="popover"]').popover()
-})
+});
 
 $(function() {
     // prevents jumping
@@ -13,4 +13,22 @@ $(function() {
         return true;
     });
     $(".pop-me-over").popover();
+});
+
+$('input:disabled').after(function (e) {
+    var d = $("<div>");
+    var i = $(this);
+    d.css({
+        height: i.outerHeight(),
+        width: i.outerWidth(),
+        position: "absolute"
+    });
+    d.css(i.offset());
+    d.attr("title", i.attr("title"));
+    d.tooltip();
+    return d;
+});
+
+$(window).load(function(){
+    $('.pops').popover();
 });
